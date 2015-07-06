6309088c4246c93c6fcf843498e4a010d87c6e0c
TonyBarnett <tony.barnett@ukplc.net>
2015-07-06 16:11:49 +0100

    everyone loves tests

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -2,6 +2,7 @@ from mock import patch
 import unittest
 from uncertainty.distribution import Distribution, LogNormalDistribution
 
+
 class FactoryTest(unittest.TestCase):
     def test_create(self):
         m = LogNormalDistribution.get_distribution(1, 2)
@@ -10,3 +11,13 @@ class FactoryTest(unittest.TestCase):
     def test_create_from_regression_coefficients_type(self):
         m = LogNormalDistribution.get_distribution_from_regression_coefficients(1, 2)
         self.assertEqual(type(m), LogNormalDistribution)
+
+    def test_obeservation_type(self):
+        m = LogNormalDistribution(1, 2)
+        observation = m.get_observation()
+        self.assertEqual(type(observation), float)
+
+    def test_observation(self):
+        m = LogNormalDistribution(0.1, 0.1)
+        observation = m.get_observation()
+        self.assertAlmostEqual(observation, 1, places=0)
