9374ac39b4778786e3029bf3bd7c8f9a4e439092
TonyBarnett <tony.barnett@ukplc.net>
2015-07-06 16:11:41 +0100

    get distribution from list of x coordinates

--- uncertainty/distribution.py
+++ uncertainty/distribution.py
@@ -1,5 +1,7 @@
 import random
 
+from .source_uncertainty.uncertainty_functions import get_mean, get_standard_deviation
+
 
 class Distribution:
     def __init__(self, mu: float, sigma: float):
@@ -13,18 +15,15 @@ class Distribution:
     def get_observation(self):
         raise NotImplementedError()
 
+    def get_pdf_line(self, x: list) -> list:
+        raise NotImplementedError()
 
 class LogNormalDistribution(Distribution):
     def __init__(self, mu, sigma):
         super().__init__(mu, sigma)
 
-    # @classmethod
-    # def get_distribution(cls ,mu, sigma):
-    #     distribution = super(LogNormalDistribution, cls).get_distribution(mu, sigma)
-    #     return distribution
-
     def get_observation(self):
-        random.lognormvariate(self.mu, self.sigma)
+        return random.lognormvariate(self.mu, self.sigma)
 
     @classmethod
     def get_distribution_from_regression_coefficients(cls, a, b):
@@ -35,5 +34,13 @@ class LogNormalDistribution(Distribution):
         """
         mu = 1
         sigma = 3
-        distribution = super(LogNormalDistribution, cls).get_distribution(mu, sigma)
-        return distribution
+        return super(LogNormalDistribution, cls).get_distribution(mu, sigma)
+
+    @classmethod
+    def get_distribution_from_coordinate_lists(cls, x: list, y: list):
+        mu = get_mean(y)
+        sigma = get_standard_deviation(y)
+        return super(LogNormalDistribution, cls).get_distribution(mu, sigma)
+
+    def get_pdf_line(self, x: list) -> list:
+        pass
