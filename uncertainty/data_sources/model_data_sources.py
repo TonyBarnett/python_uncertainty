2475823a4aa737fc47a34cde8d68695540654109
TonyBarnett <tony.barnett@ukplc.net>
2015-07-09 16:12:38 +0100

    easier source data getter

--- uncertainty/data_sources/model_data_sources.py
+++ uncertainty/data_sources/model_data_sources.py
@@ -1,5 +1,5 @@
 import pymongo
-from .sql import _read_from_sql, _build_source_query
+from .sql import read_from_sql, build_source_query
 
 
 def get_map(map_collection: str='Plain_KNN_Without_Ancestors_k_3'):
@@ -17,26 +17,39 @@ def get_map(map_collection: str='Plain_KNN_Without_Ancestors_k_3'):
 
 def _build_consumption_query(region=None, year=None) -> str:
     query = "SELECT intYear, strRegion, strSystem, strSourceValue, strTargetValue, fltConsumption FROM sor.Consumption"
-    return _build_source_query(query, region, year)
+    return build_source_query(query, region, year)
 
 
 def _build_production_query(region=None, year=None) -> str:
     query = "SELECT intYear, strRegion, strSystem, strSourceValue, strTargetValue, fltProduction FROM sor.production"
-    return _build_source_query(query, region, year)
+    return build_source_query(query, region, year)
 
 
 def _build_emissions_query(region=None, year=None) -> str:
-    query = "SELECT intYear, strRegion, strSystem, strValue, fltEmissions FROM sor.Emissions"
-    return _build_source_query(query, region, year)
+    query = "SELECT intYear, strRegion, strSystem, strValue, fltEmission FROM sor.Emission"
+    return build_source_query(query, region, year)
 
 
-def get_source_consumption(region=None, year=None) -> dict():
-    return _read_from_sql(_build_consumption_query(region, year), db="IOModel")
+def get_source_matrix_of_type(type_, region=None, year=None) -> tuple:
+    if type_ == "consumption":
+        return get_source_consumption(region, year)
+    elif type_ == "production":
+        return get_source_production(region, year)
+    elif type_ == "emissions":
+        return get_source_emissions(region, year)
+    elif type_ == "export":
+        raise NotImplementedError("go do it, lazy programmer")
+    else:
+        raise ValueError("{0} type not recognised".format(type_))
 
 
-def get_source_production(region=None, year=None) -> dict():
-    return _read_from_sql(_build_production_query(region, year), db="IOModel")
+def get_source_consumption(region=None, year=None) -> tuple:
+    return read_from_sql(_build_consumption_query(region, year), db="IOModel")
 
 
-def get_source_emissions(region=None, year=None) -> dict():
-    return _read_from_sql(_build_emissions_query(region, year), db="IOModel")
+def get_source_production(region=None, year=None) -> tuple:
+    return read_from_sql(_build_production_query(region, year), db="IOModel")
+
+
+def get_source_emissions(region=None, year=None) -> tuple:
+    return read_from_sql(_build_emissions_query(region, year), db="IOModel")
