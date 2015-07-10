fb768e4dca5837bd33c556f0e42f305b4a995024
TonyBarnett <tony.barnett@ukplc.net>
2015-07-10 12:24:48 +0100

    capitals matter

--- uncertainty/source_uncertainty_distribution/get_distribution.py
+++ uncertainty/source_uncertainty_distribution/get_distribution.py
@@ -14,16 +14,16 @@ from uncertainty.source_uncertainty_distribution.get_uk_emissions_uncertainty_di
 
 _distribution_region_type_functions = {
     "consumption": {
-        "eu": get_eu_consumption_uncertainty_distribution,
-        "uk": get_uk_consumption_uncertainty_distribution
+        "EU": get_eu_consumption_uncertainty_distribution,
+        "UK": get_uk_consumption_uncertainty_distribution
     },
     "production": {
-        "eu": get_eu_supply_uncertainty_distribution,
-        "uk": get_uk_supply_uncertainty_distribution
+        "EU": get_eu_supply_uncertainty_distribution,
+        "UK": get_uk_supply_uncertainty_distribution
     },
     "emissions": {
-        "eu": None,
-        "uk": get_uk_emissions_distribution
+        "EU": None,
+        "UK": get_uk_emissions_distribution
     },
     "import":  get_imports_uncertainty_distribution,
 }
