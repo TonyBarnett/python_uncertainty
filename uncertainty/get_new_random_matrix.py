311e59870d99f54ebac31b7a70ac95c38b5df72f
TonyBarnett <tony.barnett@ukplc.net>
2015-07-10 16:16:29 +0100

    get a perturbed vector

--- uncertainty/get_new_random_matrix.py
+++ uncertainty/get_new_random_matrix.py
@@ -1,4 +1,4 @@
-from uncertainty.matrix import Matrix
+from uncertainty.matrix import Matrix, Vector
 
 
 def get_perturbation_from_distribution(distribution) -> float:
@@ -9,6 +9,16 @@ def _get_log_normal_distribution(a, b):
     pass
 
 
+def get_new_perturbed_vector(vec: Vector, distribution):
+    perturbed_vector = Vector()
+
+    for row_key in vec.keys:
+        perturbed_value = vec.get_element(row_key) + get_perturbation_from_distribution(distribution)
+        perturbed_vector.set_element(row_key, value=perturbed_value)
+
+    return perturbed_vector
+
+
 def get_new_perturbed_matrix(mat: Matrix, distribution) -> Matrix:
     # TODO add small value to each element in Matrix
     # work out logNormal distribution from y = a ln(x) + b,
@@ -17,6 +27,6 @@ def get_new_perturbed_matrix(mat: Matrix, distribution) -> Matrix:
 
     for row_key in mat.row_keys:
         for column_key in mat.column_keys:
-            perturbed_value = mat.get_element(row_key,column_key) + get_perturbation_from_distribution(distribution)
-            perturbed_matrix.set_element(row_key=row_key, col_key= column_key, value=perturbed_value)
+            perturbed_value = mat.get_element(row_key, column_key) + get_perturbation_from_distribution(distribution)
+            perturbed_matrix.set_element(row_key=row_key, col_key=column_key, value=perturbed_value)
     return perturbed_matrix
