601cbf5a5ee28751789632e7d8cc15aec4f8d6c7
TonyBarnett <tony.barnett@ukplc.net>
2015-07-13 10:48:42 +0100

    run single region model

--- uncertainty/Monte_Carlo/single_region_model.py
+++ uncertainty/Monte_Carlo/single_region_model.py
@@ -1,7 +1,8 @@
 from uncertainty.matrix import Matrix, Vector
+from models import single_region
 
 
 def run_single_region_model(uk_production: Matrix,
                             uk_consumption: Matrix,
                             uk_emissions: Vector) -> Vector:
-    pass
+    single_region.run_model(uk_consumption, uk_production, uk_emissions)
