247fc63692dbec5181d0578be9647bd4db369840
TonyBarnett <tony.barnett@ukplc.net>
2015-07-13 11:07:15 +0100

    whoops

--- uncertainty/Monte_Carlo/single_region_model.py
+++ uncertainty/Monte_Carlo/single_region_model.py
@@ -5,4 +5,4 @@ from models import single_region
 def run_single_region_model(uk_production: Matrix,
                             uk_consumption: Matrix,
                             uk_emissions: Vector) -> Vector:
-    single_region.run_model(uk_consumption, uk_production, uk_emissions)
+    return single_region.run_model(uk_consumption, uk_production, uk_emissions)
