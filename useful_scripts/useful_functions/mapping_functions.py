48a61c3d6cdfff685fc9add033667fc0b6e9c948
TonyBarnett <tony.barnett@ukplc.net>
2015-07-23 09:41:37 +0100

    move things about a bit

---
+++ useful_scripts/useful_functions/mapping_functions.py
@@ -0,0 +1,17 @@
+from uncertainty.source_uncertainty_distribution.uncertainty_functions import get_ancestors_and_self
+
+
+def map_things_together(thing1: dict, thing2: dict) -> list:
+    """
+    thing1's keys must be ancestor or selfs of thing2's keys
+    :param thing1:
+    :param thing2:
+    :return: list of tuples
+    """
+    x_y = list()
+    error_key_map = {error_key: get_ancestors_and_self(error_key) for error_key in thing2.keys()}
+    for error_key, error in thing2.items():
+        for ancestor in error_key_map[error_key]:
+            if ancestor in thing1.keys():
+                x_y.append((thing1[ancestor], error))
+    return sorted(x_y)
--- useful_scripts/useful_functions/plot_functions.py
+++ useful_scripts/useful_functions/plot_functions.py
@@ -23,3 +23,17 @@ def add_regression_line_to_graph(a: float, b: float, x: list, colour: str="x"):
     y = [a * ln(x_i) + b for x_i in sorted_x]
     y = [y_i if y_i > 0.001 else 0.001 for y_i in y]
     pyplot.plot(sorted_x, y, colour)
+
+
+def plot_x_y(x_y: list):
+    pyplot.figure()
+    x = list()
+    y = list()
+    for x_i, y_i in x_y:
+        x.append(x_i)
+        y.append((y_i + x_i) / x_i)
+
+    pyplot.plot(x, y, "kx")
+    pyplot.xlabel("UK supply")
+    pyplot.ylabel("(\Delta x + x) / x")
+    pyplot.tight_layout(pad=0.5)
