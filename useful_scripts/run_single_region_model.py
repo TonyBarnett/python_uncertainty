836e3d58807c096081fc207ec3fa6a1484c3ad3d
TonyBarnett <tony.barnett@ukplc.net>
2015-08-10 21:40:05 +0100

    maybe useful?

---
+++ useful_scripts/run_single_region_model.py
@@ -0,0 +1,31 @@
+from run_uncertainty_model import create_and_populate_source_data
+from uncertainty.data_mapping import map_source_data_matrix
+
+
+def estimate_unknowns(year: int):
+    raise NotImplementedError()
+
+
+def map_data(year: int):
+    raise NotImplementedError()
+
+
+def run_model(year: int):
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    # get data
+    # estimate unknowns
+    # map data
+    # run model
+    year = 2008
+    sd = create_and_populate_source_data(year)
+    source_data = list()
+    for source_thing in sd:
+        if source_thing.region == "UK":
+            source_data.append(source_thing)
+
+    mapped_data = map_source_data_matrix(source_data)
+
+    print(1)
